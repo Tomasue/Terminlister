5d7c240b99d23b212c9690f1b89297e7a7e91fa4
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 14:32:25 +0100

    Website more or less done
    might need to fix a few things later.

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,19 +1,12 @@
 $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
-let current = document.getElementsByClassName('set-orange-theme');
-current = sessionStorage.setItem('current', current);
-$('.set-' + current).each(item => {
-    $(item).addClass('active');
-});
+
 sessionStorage.getItem('current');
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
     primaryColor = sessionStorage.setItem('bg', primaryColor);
     secondaryColor = sessionStorage.setItem('cc', secondaryColor);
-
-    $(".header--theme-button").removeClass("active");
-    $(this).addClass("active");
 
     if (sessionStorage.getItem('bg') === primaryColor) {
         sessionStorage.setItem('bg', primaryColor);
