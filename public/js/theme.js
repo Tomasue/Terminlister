bce77646f479b811a3aa3cfd1db63b51a51c916e
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 11:59:10 +0100

    test
    test

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,17 +1,17 @@
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
-    localStorage.setItem('prime', primaryColor);
-    localStorage.setItem('second', secondaryColor);
-    console.log('nuggest');
+    localStorage.primary = primaryColor;
+    localStorage.second = secondaryColor;
+
 
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
 
 
     if (typeof(Storage) !== "undefined") {
-        $(document.body).css("--primary-color", localStorage.getItem('prime'));
-        $(document.body).css("--secondary-color", localStorage.getItem('second'));
+        $(document.body).css("--primary-color", localStorage.primary);
+        $(document.body).css("--secondary-color", localStorage.second);
     } else {
         $(document.body).css("--primary-color", primaryColor);
         $(document.body).css("--secondary-color", secondaryColor);
