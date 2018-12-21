b532e438ac221c72481c2dea5cc371c9a1e5f2e4
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 16:11:00 +0100

    Fixed theme

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,23 +1,23 @@
-$(document.body).css("--primary-color", sessionStorage.getItem('bg'));
-$(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
+$(document.body).css("--primary-color", localStorage.getItem('bg'));
+$(document.body).css('--secondary-color', localStorage.getItem('cc'));
 
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
-    primaryColor = sessionStorage.setItem('bg', primaryColor);
-    secondaryColor = sessionStorage.setItem('cc', secondaryColor);
+    primaryColor = localStorage.setItem('bg', primaryColor);
+    secondaryColor = localStorage.setItem('cc', secondaryColor);
 
 
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
 
-    if (sessionStorage.getItem('bg') === primaryColor) {
-        sessionStorage.setItem('bg', primaryColor);
-        sessionStorage.setItem('cc', secondaryColor);
+    if (localStorage.getItem('bg') === primaryColor) {
+        localStorage.setItem('bg', primaryColor);
+        localStorage.setItem('cc', secondaryColor);
     }
 
-    $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
-    $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
+    $(document.body).css("--primary-color", localStorage.getItem('bg'));
+    $(document.body).css('--secondary-color', localStorage.getItem('cc'));
 
 });
 
