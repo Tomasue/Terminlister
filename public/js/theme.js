0d4a8f4c19732977f8e1aa63ce7e675e0cf81a70
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 14:27:18 +0100

    test

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,5 +1,10 @@
 $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
+let current = document.getElementsByClassName('set-orange-theme');
+current = sessionStorage.setItem('current', current);
+$('.set-' + current).each(item => {
+    $(item).addClass('active');
+});
 sessionStorage.getItem('current');
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
@@ -7,23 +12,13 @@ $(".header--theme-button").on("click", function() {
     primaryColor = sessionStorage.setItem('bg', primaryColor);
     secondaryColor = sessionStorage.setItem('cc', secondaryColor);
 
-
-
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
-    let test = $(this).classList.contains('set');
-    console.log(test);
-    let current = sessionStorage.getItem('current');
-    current = sessionStorage.setItem('current', selected);
-    $('.set-' + current).each(item => {
-        $(item).addClass('active');
-    });
 
-
-        if (sessionStorage.getItem('bg') === primaryColor) {
-            sessionStorage.setItem('bg', primaryColor);
-            sessionStorage.setItem('cc', secondaryColor);
-        }
+    if (sessionStorage.getItem('bg') === primaryColor) {
+        sessionStorage.setItem('bg', primaryColor);
+        sessionStorage.setItem('cc', secondaryColor);
+    }
 
     $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
     $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
