08a10fbc045cd322c613fb2816794d5676ea5432
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 15:34:09 +0100

    Done for now

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,20 +1,15 @@
 $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
-sessionStorage.getItem('current');
+
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
     primaryColor = sessionStorage.setItem('bg', primaryColor);
     secondaryColor = sessionStorage.setItem('cc', secondaryColor);
 
+
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
-    let current = $(this).attr('class');
-    current.toString();
-    $('.set-' + current).each(item => {
-        $(item).addClass('active');
-        sessionStorage.setItem('current', current);
-    });
 
     if (sessionStorage.getItem('bg') === primaryColor) {
         sessionStorage.setItem('bg', primaryColor);
@@ -23,7 +18,6 @@ $(".header--theme-button").on("click", function() {
 
     $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
     $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
-
 
 });
 
