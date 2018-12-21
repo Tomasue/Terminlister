125112e5e51b9c9d4b5df22e14196e712708ad86
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 13:56:56 +0100

    test

--- public/js/theme.js
+++ public/js/theme.js
@@ -2,15 +2,20 @@ $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
 localStorage.getItem('active');
 $(".header--theme-button").on("click", function() {
-
-
-    $(".header--theme-button").removeClass("active");
-    $(this).addClass("active");
-    localStorage.setItem('active', $(this).addClass("active"));
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
     primaryColor = sessionStorage.setItem('bg', primaryColor);
     secondaryColor = sessionStorage.setItem('cc', secondaryColor);
+
+    if(primaryColor === 'orange') {
+        $(this).addClass("active");
+    }
+
+    $(".header--theme-button").removeClass("active");
+    $(this).addClass("active");
+
+
+
 
         if (sessionStorage.getItem('bg') === primaryColor) {
             sessionStorage.setItem('bg', primaryColor);
