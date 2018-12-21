76afe2250a77933343b30de21c20e77db0b4efae
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 10:12:32 +0100

    tes

--- public/js/theme.js
+++ public/js/theme.js
@@ -3,7 +3,7 @@ $(".header--theme-button").on("click", function() {
     var secondaryColor = $(this).css("--theme-secondary");
     localStorage.setItem('primaryCol', primaryColor);
     localStorage.setItem('secondCol', secondaryColor);
-
+    console.log('nuggest');
 
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
