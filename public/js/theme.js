3545ad0d60d475fd12314fdff28e8ebdb0ac843c
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 13:48:12 +0100

    testing js

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,11 +1,12 @@
 $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
+
 $(".header--theme-button").on("click", function() {
 
 
     $(".header--theme-button").removeClass("active");
     $(this).addClass("active");
-
+    localStorage.setItem('active', $(this).addClass("active"));
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
     primaryColor = sessionStorage.setItem('bg', primaryColor);
