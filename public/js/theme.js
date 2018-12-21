4e14b0007184f366be12eb5576a6c2e19adda81e
Tomasue <thomas.richvoldsen@hotmail.com>
2018-12-21 14:54:49 +0100

    teseru

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,7 +1,5 @@
 $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
 $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
-
-
 sessionStorage.getItem('current');
 $(".header--theme-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
@@ -9,19 +7,22 @@ $(".header--theme-button").on("click", function() {
     primaryColor = sessionStorage.setItem('bg', primaryColor);
     secondaryColor = sessionStorage.setItem('cc', secondaryColor);
 
-    if (sessionStorage.getItem('bg') === primaryColor) {
-        sessionStorage.setItem('bg', primaryColor);
-        sessionStorage.setItem('cc', secondaryColor);
-    }
+    $(".header--theme-button").removeClass("active");
+    $(this).addClass("active");
     let current = $(this).attr('class').toString();
-
     $('.set-' + current).each(item => {
         $(item).addClass('active');
         sessionStorage.setItem('current', current);
     });
 
+    if (sessionStorage.getItem('bg') === primaryColor) {
+        sessionStorage.setItem('bg', primaryColor);
+        sessionStorage.setItem('cc', secondaryColor);
+    }
+
     $(document.body).css("--primary-color", sessionStorage.getItem('bg'));
     $(document.body).css('--secondary-color', sessionStorage.getItem('cc'));
+
 
 });
 
